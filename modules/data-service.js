f7f69b43fec99665a64b0fd9abc0dcad341b454b
Arthur Dibe <arthurverissimo82@gmail.com>
2021-05-31 22:44:19 -0400

    added the link to database into data-service.js

--- modules/data-service.js
+++ modules/data-service.js
@@ -13,7 +13,8 @@ const User = require("../Models/userSchema");
 // function initialize creates the connection between server and MongoDB database
 const initialize = ()=>{
 
-    mongoose.connect(keys.MONGO_DB_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+    //mongoose.connect(keys.MONGO_DB_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+    mongoose.connect(`mongodb+srv://${process.env.MONGODB_USRNAME}:${process.env.MONGODB_PASSWD}@${process.env.MONGODB_CLUSTER}/${process.env.MONGODB_DATABASE}?${process.env.MONGODB_ENDLINK}`, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(()=>{
         console.log(chalk.yellow(`Happy Bidding database: `) + chalk.green(`SCCESSFULLY connected to the database !`));
         console.log(chalk.blue(`------------------------------------------------------------------------------------`));
