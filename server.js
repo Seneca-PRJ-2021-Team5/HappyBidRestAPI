0e2ef51c647690526c034b2e359a153f38ab5b1f
Arthur Dibe <dibearthur@gmail.com>
2021-07-24 16:40:19 -0400

    server.js: Created route for deleting an auction

--- server.js
+++ server.js
@@ -142,6 +142,10 @@ app.post("/api/auctioneer/replyProblem/:id",(req,res)=>{
     dataService.replyProblem(req.body,req.params.id,res)
 })
 
+app.delete('/api/auctioneer/deleteAuction/:id', function (req, res) {
+    dataService.deleteAuction(req.params.id,res)
+})
+
 
 //--------------------------------------------------------
 
